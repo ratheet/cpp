979cf1c9a0c2ed243caf87d879d69c5883885356
rath <ratheet@gmail.com>
2017-03-04 22:06:36 +0000

    Starting to flesh out DirectedGraph based on print and vertex count tests.

--- graphs.h
+++ graphs.h
@@ -1,22 +1,60 @@
+#include <sstream>
 #include <string>
+#include <vector>
 
+using std::ostringstream; 
 using std::string;
+using std::vector;
 using Value = std::pair<string, int>;
 
 template<typename T>
 concept bool Stringable = requires(T a) {
   { a.to_string() } -> string;
 };
-  
-template<typename T>
-concept bool Graph = Stringable<T>;
-  /*&&
-  //  requires(T a) {
-};
-  */
 
-struct DirectedGraph {
-  string to_string() {
-    return "hello there!\n";
-  }
+template<typename T>
+concept bool Vertex_ptr = Stringable<T>;
+  
+template<typename G>
+concept bool Graph =
+  Stringable<G> &&
+  requires(G g) {
+  { g.vertex_count() } -> int;
 };
+
+class Vertex {
+ public:
+  Vertex(Value value) : value_(value) {}
+  string to_string() const {
+    ostringstream oss;
+    oss << "(" << this->value_.first << ", " << this->value_.second << ")";
+    return oss.str();
+  }
+  
+ private:
+  Value value_;
+};
+
+template<Vertex_ptr V>
+class DirectedGraph {
+ public:
+  string to_string() const {
+    string str_value = "Graph:\n";
+    for (const Vertex& v : vertices_) {
+      return str_value += v.to_string();
+    }
+    return str_value;
+  }
+
+  void add(V v) {
+    vertices_.push_back(v);
+  }
+  
+  int vertex_count() {
+    return vertices_.size();
+  }
+  
+ private:
+  vector<V> vertices_;
+};
+
--- graphs_test.cc
+++ graphs_test.cc
@@ -1,12 +1,42 @@
+#include <cassert>
 #include <iostream>
 #include <vector>
 #include "graphs.h"
 
+using std::cout;
+using std::make_pair;
+
 void print(Graph g) {
-  std::cout << g.to_string() << "\n";
+  cout << g.to_string() << "\n";
+}
+
+int count_vertices(Graph g) {
+  return g.vertex_count();
+}
+
+void test_print() {
+  DirectedGraph<Vertex> dg;
+  Vertex v1(make_pair("A", 1));
+  dg.add(v1);
+  print(dg);  
+}
+
+void test_count_vertices() {
+  DirectedGraph<Vertex> dg;
+  Vertex v1(make_pair("A", 1));
+  Vertex v2(make_pair("B", 2));
+  Vertex v3(make_pair("C", 3));
+  dg.add(v1);
+  dg.add(v2);
+  dg.add(v3);
+  assert(count_vertices(dg) == 3);
 }
 
 int main() {
-  DirectedGraph dg;
-  print(dg);
+  assert(__cpp_concepts >= 201500); // check compiled with -fconcepts
+  assert(__cplusplus >= 201500);    // check compiled with --std=c++1z
+
+  test_print();
+  test_count_vertices();
+  cout << "All tests passed.\n";
 }
